e6a8d261cffcb8c41fa6216aea377203a8054358
Daliana-Solis <dsolisso@uci.edu>
2023-09-28 17:05:29 -0500

    Create sqlite database

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "myapp123" #passw
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class Form(db.Model): #Set up database
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
+
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -18,4 +33,7 @@ def index():
 
 
 
-app.run(debug=True, port=5001)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=5001)
